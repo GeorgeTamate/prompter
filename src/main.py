9d72f61d527e0b8c04c971f2cf06ac4f63aa632d
George Tamate <georgetamate@gmail.com>
2024-08-26 00:09:32 -0400

    Add explanatory comments to the main entry point of the program.

--- src/main.py
+++ src/main.py
@@ -13,15 +13,19 @@ try:
     # Intance custom client
     gpt_client = GPTClient(gpt_client_config)
 
+    # Simulate failed prompting
     gpt_client.prompt_model()
 
+    # Add initial messages and system context to history
     messages = gpt_client.add_user_message_under_new_context(
         context_text="You are a helpful assistant that responds only in Spanish regardless the input language.",
         message_content="Describe the city of New York in 3 sentences."
     )
 
+    # Prompting with actual messages (also prints received message from API)
     gpt_client.prompt_model(log_completion=True)
 
+    # Display resulting history
     message_history = gpt_client.get_message_history()
     print("MESSAGE HISTORY:")
     print(json.dumps(obj=message_history, indent=4))
